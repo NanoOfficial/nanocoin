8175a02f7b50ef08c42b50b69ce0ef482d1258a4
krishpranav <krisna.pranav@gmail.com>
2023-06-24 11:53:49 +0530

    boost: flat_buffer

---
+++ src/boost/flat_buffer.hpp
@@ -0,0 +1,18 @@
+/**
+ * @file flat_buffer.hpp
+ * @author Krisna Pranav
+ * @brief Flat Buffer 
+ * @version 1.0
+ * @date 2023-06-24
+ * 
+ * @copyright Copyright (c) 2023 Krisna Pranav, NanoBlocksDevelopers
+ * 
+ */
+
+#pragma once
+
+#include "macros_warnings.h"
+
+DISABLE_BEAST_WARNINGS
+#include <boost/beast/core/flat_buffer.hpp>
+REENABLE_WARNINGS
