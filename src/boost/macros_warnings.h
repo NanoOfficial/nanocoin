dbe9ae255f5c390e536163c9f19bf0c66ef84f1b
krishpranav <krisna.pranav@gmail.com>
2023-06-24 11:40:24 +0530

    src: boost + README

---
+++ src/boost/macros_warnings.h
@@ -0,0 +1,41 @@
+/**
+ * @file macros_warnings.h
+ * @author Krisna Pranav
+ * @brief Macro Warning
+ * @version 1.0
+ * @date 2023-06-24
+ * 
+ * @copyright Copyright (c) 2023 Krisna Pranav, NanoBlocksDevelopers
+ * 
+ */
+
+#pragma once
+
+#ifdef _WIN32
+#define DISABLE_ASIO_WARNINGS           \
+	__pragma (warning (push))           \
+	__pragma (warning (disable : 4191)) \
+	__pragma (warning (disable : 4242))
+
+#else
+#define DISABLE_ASIO_WARNINGS
+#endif
+
+#ifdef _WIN32
+#define REENABLE_WARNINGS \
+	__pragma (warning (pop))
+#else
+#define REENABLE_WARNINGS
+#endif
+
+#define DISABLE_BEAST_WARNINGS DISABLE_ASIO_WARNINGS
+
+#ifdef _WIN32
+#define DISABLE_PROCESS_WARNINGS        \
+	__pragma (warning (push))           \
+	__pragma (warning (disable : 4191)) \
+	__pragma (warning (disable : 4242)) \
+	__pragma (warning (disable : 4244))
+#else
+#define DISABLE_PROCESS_WARNINGS
+#endif
