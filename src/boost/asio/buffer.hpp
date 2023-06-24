3e799320221c627a83e82b57b1bd8f3eb84f5bba
krishpranav <krisna.pranav@gmail.com>
2023-06-24 11:59:28 +0530

    boost: asio[buffer]

---
+++ src/boost/asio/buffer.hpp
@@ -0,0 +1,18 @@
+/**
+ * @file buffer.hpp
+ * @author Krisna Pranav
+ * @brief Buffer 
+ * @version 0.1
+ * @date 2023-06-24
+ * 
+ * @copyright Copyright (c) 2023 Krisna Pranav, NanoBlocksDevelopers
+ * 
+ */
+
+#pragma once
+
+#include "macros_warnings.h"
+
+DISABLE_ASIO_WARNINGS
+#include <boost/asio/buffer.hpp>
+REENABLE_WARNINGS
