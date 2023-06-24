6236749e52ea6dd49d4fd4d418222586f7344964
krishpranav <krisna.pranav@gmail.com>
2023-06-24 12:07:00 +0530

    boost: asio[post]

---
+++ src/boost/asio/post.hpp
@@ -0,0 +1,18 @@
+/**
+ * @file post.hpp
+ * @author Krisna Pranav
+ * @brief Post
+ * @version 1.0
+ * @date 2023-06-24
+ * 
+ * @copyright Copyright (c) 2023 Krisna Pranav, NanoBlocksDevelopers
+ * 
+ */
+
+#pragma once
+
+#include "../macros_warnings.h"
+
+DISABLE_ASIO_WARNINGS
+#include <boost/asio/post.hpp>
+REENABLE_WARNINGS
